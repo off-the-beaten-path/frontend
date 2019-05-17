e37a51aab165951b8da1ac8ab5fdbd26bce1b1a9
Tristan K <tristanmkernan@gmail.com>
2019-05-17 10:34:25 -0400

    fix: incorrect prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   VERSION: '0.1',
   production: true,
-  api: 'https://otbpbackend.tmk.name',
-  whitelistedDomains: ['otbpbackend.tmk.name'],
+  api: 'https://otbpapi.tmk.name',
+  whitelistedDomains: ['otbpapi.tmk.name'],
   blacklistedRoutes: [
-    'https://otbpbackend.tmk.name:8099/user/login',
-    'https://otbpbackend.tmk.name:8099/user/refresh',
-    'https://otbpbackend.tmk.name:8099/user/register',
+    'https://otbpapi.tmk.name/user/login',
+    'https://otbpapi.tmk.name/user/refresh',
+    'https://otbpapi.tmk.name/user/register',
   ]
 };
