4d9a30d22773423c180da0f04f8093f88dd3303c
Tristan K <tristanmkernan@gmail.com>
2019-05-29 13:53:50 -0400

    fix: geolocation in play component

--- src/app/services/geolocation.service.ts
+++ src/app/services/geolocation.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject, ReplaySubject, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { ILatLngPosition } from '../models';
-import { take } from 'rxjs/operators';
 
 const positionOptions: PositionOptions = {
   enableHighAccuracy: true,
@@ -14,89 +13,74 @@ const positionOptions: PositionOptions = {
 })
 export class GeolocationService {
 
-  private lastKnownPosition: null | ILatLngPosition = null;
-  private subject: null | ReplaySubject<ILatLngPosition> = null;
-  private watchId: null | number = null;
-
-  constructor() {
-  }
+  constructor() {}
 
   getCurrentPosition(): Observable<ILatLngPosition> {
-    if (null !== this.subject) {
-      return this.subject.pipe(take(1));
-    } else if ('geolocation' in navigator) {
-      const subject = new Subject<ILatLngPosition>();
+    if ('geolocation' in navigator) {
+      return new Observable(
+        observable => {
+          navigator
+            .geolocation
+            .getCurrentPosition(
+              success => {
+                const latestPosition: ILatLngPosition = {
+                  lat: success.coords.latitude,
+                  lng: success.coords.longitude
+                };
 
-      navigator
-        .geolocation
-        .getCurrentPosition(
-          success => {
-            const latestPosition: ILatLngPosition = {
-              lat: success.coords.latitude,
-              lng: success.coords.longitude
-            };
+                observable.next(latestPosition);
+                observable.complete();
+              },
+              error => {
+                if (null !== error) {
+                  observable.error(error);
+                } else {
+                  observable.error('Generic error in getting location.');
+                }
 
-            subject.next(latestPosition);
-            this.lastKnownPosition = latestPosition;
-          },
-          error => {
-            if (null !== error) {
-              subject.error(error);
-            } else {
-              subject.error('Generic error in getting location.');
-            }
-          },
-          positionOptions
-        );
-
-      return subject.pipe(take(1));
+                observable.complete();
+              },
+              positionOptions
+            );
+        }
+      );
     }
 
     return throwError('getCurrentPosition() unavailable');
   }
 
   watchPosition(): Observable<ILatLngPosition> {
-    if (null !== this.subject) {
-      return this.subject.asObservable();
-    } else if ('geolocation' in navigator) {
-      this.subject = new ReplaySubject<ILatLngPosition>(1);
+    if ('geolocation' in navigator) {
+      return new Observable(
+        observable => {
+          const watchId = navigator
+            .geolocation
+            .watchPosition(
+              success => {
+                const latestPosition: ILatLngPosition = {
+                  lat: success.coords.latitude,
+                  lng: success.coords.longitude
+                };
 
-      this.watchId = navigator
-        .geolocation
-        .watchPosition(
-          success => {
-            const latestPosition: ILatLngPosition = {
-              lat: success.coords.latitude,
-              lng: success.coords.longitude
-            };
+                observable.next(latestPosition);
+              },
+              error => {
+                if (null !== error) {
+                  observable.error(error);
+                } else {
+                  observable.error('Generic error in getting location.');
+                }
 
-            this.subject.next(latestPosition);
-            this.lastKnownPosition = latestPosition;
-          },
-          error => {
-            if (null !== error) {
-              this.subject.error(error);
-            } else {
-              this.subject.error('Generic error in getting location.');
-            }
-          },
-          positionOptions
-        );
+                observable.complete();
+              },
+              positionOptions
+            );
 
-      return this.subject.asObservable();
+          return () => navigator.geolocation.clearWatch(watchId);
+        }
+      );
     }
 
     return throwError('watchPosition() unavailable');
-  }
-
-  stopWatching(): void {
-    if (null !== this.watchId) {
-      navigator.geolocation.clearWatch(this.watchId);
-    }
-
-    if (null !== this.subject) {
-      this.subject.complete();
-      this.subject = null;
-    }
   }
 }
